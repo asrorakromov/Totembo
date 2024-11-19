1d75aa00cbd4035db365a8bc24625bc6a0bdd625
asrorakromov <asrorakromov989@gmail.com>
2024-11-19 21:27:38 +0300

    Работа с вьюшкой

--- watches/views.py
+++ watches/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from watches.models import Product, Categories
 
 
-def index(request):
-    return render(request, 'index.html')
+class ProductListView(ListView):
+    model = Product
+    context_object_name = 'categories'
+    template_name = 'watches/index.html'
+    extra_context = {
+        'title': 'ТОТЕМВО'
+    }
+
+    def get_queryset(self):
+        categories = Categories.objects.all()
+        return categories
